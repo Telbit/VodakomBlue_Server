591c16e91332b18e6062233074135fb26dc38b0b
kalacs <divixed12@gmail.com>
2021-03-04 12:26:42 +0100

    Add IHomePhoneRepository

---
+++ Repositories/IHomePhoneRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Repositories
+{
+    public interface IHomePhoneRepository
+    {
+        Task<HomePhone> GetHomePhoneAsync(int homePhoneId);
+        Task AddHomePhoneAsync(HomePhone newHomePhone);
+        Task DeleteHomePhoneAsync(int homePhoneId);
+    }
+}
