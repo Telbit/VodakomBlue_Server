0cad54327f853cb36808a1ed13911447bef39174
kalacs <divixed12@gmail.com>
2021-03-04 13:30:25 +0100

    Add IHomeServiceRepository

---
+++ Repositories/IHomeServiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Repositories
+{
+    public interface IHomeServiceRepository
+    {
+        Task<IEnumerable<HomeService>> GetServiceAsync(int userId);
+        Task AddServiceAsync(HomeService newService);
+        Task UpdateServiceAsync(HomeService homeService);
+        Task DeleteServiceAsync(int serviceId);
+    }
+}
