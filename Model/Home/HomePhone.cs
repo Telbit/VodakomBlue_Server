1a6d6397ef212dd53f980c6466a4a31c7167ed96
Telbit <t.t.bende@gmail.com>
2021-03-04 15:45:45 +0100

    Add HomePhone class

---
+++ Model/Home/HomePhone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Model.Home
+{
+    public class HomePhone
+    {
+        [Key]
+        public int Id { get; }
+
+        [MaxLength(50)]
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "TalkMinutes is Required")]
+        public int TalkMinutes { get; set; }
+
+        [Required(ErrorMessage = "Price is Required")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Date is Required")]
+        public DateTime Date { get; set; }
+    }
+}
