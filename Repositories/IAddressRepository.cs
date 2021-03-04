cc6c8820eaa50acc8c7455a3cf0341d4af4ff8f5
kalacs <divixed12@gmail.com>
2021-03-04 13:57:09 +0100

    Add IAddressrepository

---
+++ Repositories/IAddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Repositories
+{
+    public interface IAddressRepository
+    {
+        Task<IEnumerable<Address>> GetServiceAsync(int userId);
+        Task AddAddressAsync(Address newAddress);
+        Task UpdateAddressAsync(Address address);
+        Task DeleteAddressAsync(int serviceId);
+    }
+}
