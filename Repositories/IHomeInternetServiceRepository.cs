d459ba5cebe74d23cff34c5d9a69e00afe7de3ab
kalacs <divixed12@gmail.com>
2021-03-04 14:15:02 +0100

    Add IHomeInternetServiceRepository

---
+++ Repositories/IHomeInternetServiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Repositories
+{
+    public interface IHomeInternetService
+    {
+        Task<IEnumerable<HomeInternetService>> GetAllServiceAsync(int userId);
+        Task AddServiceAsync(HomeInternetService newService);
+        Task UpdateServiceAsync(HomeInternetService homeService);
+        Task DeleteServiceAsync(int serviceId);
+    }
+}
