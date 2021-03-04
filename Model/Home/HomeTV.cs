63bb3320f0094393990560068e6229195d925fe6
Telbit <t.t.bende@gmail.com>
2021-03-04 15:46:02 +0100

    Add HomeTV class

---
+++ Model/Home/HomeTV.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Model.Home
+{
+    public class HomeTV
+    {
+        [Key]
+        public int Id { get; }
+
+        [MaxLength(50)]
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is Required")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Date is Required")]
+        public DateTime Date { get; set; }
+    }
+}
