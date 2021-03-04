295f392c36aac08f7c3063c8ea55baec9ff1fb0d
kalacs <divixed12@gmail.com>
2021-03-04 12:19:07 +0100

    Add IMobileInternetPackageRepository

---
+++ Repositories/IMobileInternetPackageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Repositories
+{
+    interface IMobileInternetPackageRepository
+    {
+        Task<MobileInternetPackage> GetPackageAsync(int packageId);
+        Task AddPackageAsync(MobileInternetPackage newPackage);
+        Task DeletePackageAsync(int packageId);
+
+    }
+}
