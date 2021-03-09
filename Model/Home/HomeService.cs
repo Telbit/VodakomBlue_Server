612290ac5a356521fdc57f6d81d7a5dfeebcb071
Telbit <t.t.bende@gmail.com>
2021-03-09 14:23:36 +0100

    Add Service and HomeService abstract classes

---
+++ Model/Home/HomeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Model.Home
+{
+    public abstract class HomeService : Service
+    {
+        [Required(ErrorMessage = "Address is required")]
+        public Address Address { get; set; }
+    }
+}
