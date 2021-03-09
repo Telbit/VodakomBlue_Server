5d4af35d5d4ff08ebe6ea9e9f8ff30f6aa1852d4
Telbit <t.t.bende@gmail.com>
2021-03-09 14:27:19 +0100

    Add Home Internet, Tv and Phone service classes

---
+++ Model/Home/HomeInternetService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Model.Home
+{
+    public class HomeInternetService : HomeService
+    {
+        [Required(ErrorMessage = "Internet is required")]
+        public HomeInternet Internet { get; set; }
+    }
+}
