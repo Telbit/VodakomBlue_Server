7c6d998f1a9a0071f2abd682446c044ddb016468
kalacs <divixed12@gmail.com>
2021-03-11 15:18:23 +0100

    Add imports to Repository patter interfaces for model classes

--- Repositories/IAddressRepository.cs
+++ Repositories/IAddressRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VodakomBlue.Model;
 
 namespace VodakomBlue.Repositories
 {
--- Repositories/IContractRepository.cs
+++ Repositories/IContractRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VodakomBlue.Model;
+
 
 namespace VodakomBlue.Repositories
 {
