53462d001d32f19bf9b360acd0a2659a5d56f8e8
kalacs <divixed12@gmail.com>
2021-03-12 12:14:26 +0100

    Changed some GetUserAsync to synch. GetUser, removed unnecessary param.

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -10,9 +10,9 @@ namespace VodakomBlue.Repositories
     public interface IUserRepository
     {
         Task<User> GetUserAsync(int userId);
-        Task<User> GetUserAsync(string phoneNumber);
-        Task<User> GetUserAsync(string idCard, string fullName);
-        Task<User> GetUserAsync(string fullName,  Address clientAddress, DateTime birthDate,  string mothersName);
+        User GetUser(string phoneNumber);
+        User GetUser(string idCard, string firstName, string lastName);
+        User GetUser(Address clientAddress, DateTime birthDate,  string mothersName);
         void DeleteUser(int userId);
         void UpdateUser(User user);
         Task AddUserAsync(User newUser);
