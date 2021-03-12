225469becf703cbd220762b475addb50059e79a1
kalacs <divixed12@gmail.com>
2021-03-12 14:11:40 +0100

    Add new parameter to DeleteService() method

--- Repositories/Home/IHomeTvServiceRepository.cs
+++ Repositories/Home/IHomeTvServiceRepository.cs
@@ -12,6 +12,6 @@ namespace VodakomBlue.Repositories
         Task<IEnumerable<HomeTvService>> GetAllServiceAsync(int userId);
         Task AddServiceAsync(HomeTvService newService);
         void UpdateService(HomeTvService homeService);
-        void DeleteService(int serviceId);
+        void DeleteService(int serviceId, int userId);
     }
 }
