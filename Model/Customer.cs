204989cfe938e118bae316305b6c306b0377a259
kalacs <divixed12@gmail.com>
2021-03-16 11:37:47 +0100

    Remove unnecessary properties from Customer class

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -6,41 +6,10 @@ using System.Threading.Tasks;
 
 namespace VodakomBlue.Model
 {
-    public class Customer
+    public class Customer : User
     {
-        [Key]
-        public int Id { get; }
-
-        [Required(ErrorMessage = "First name is required")]
-        [MaxLength(30)]
-        public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Last name is required")]
-        [MaxLength(30)]
-        public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Last name is required")]
-        [EmailAddress(ErrorMessage = "Invalid email address")]
-        public string Email { get; set; }
-
         [Required(ErrorMessage = "Adresses is required")]
         public List<Address> Adresses { get; set; }
-
-        [MaxLength(30)]
-        [Required(ErrorMessage = "Id Card Number is required")]
-        public string IdCardNumber { get; set; }
-
-        [MaxLength(60)]
-        [Required(ErrorMessage = "Mother's name is required")]
-        public string MothersName { get; set; }
-
-        [Required(ErrorMessage = "Birth date is required")]
-        public DateTime BirthDate { get; set; }
-
-        [Required(ErrorMessage = "Birth date is required")]
-        [MaxLength(12), MinLength(11)]
-        public string ContactPhoneNumber { get; set; }
-
         public List<Contract> Contracts { get; set; }
     }
 }
