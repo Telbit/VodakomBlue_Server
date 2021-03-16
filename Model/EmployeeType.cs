22f55ac35869857b47a534069b6047d35670dfcf
kalacs <divixed12@gmail.com>
2021-03-16 12:43:16 +0100

    Add EmployeeType enum

---
+++ Model/EmployeeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Model
+{
+    public enum EmployeeType
+    {
+        HR,
+        Sales,
+        Manager
+    }
+}
