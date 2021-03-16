2929729524e65dffe6e93f8bcd3a72d9b9749e22
kalacs <divixed12@gmail.com>
2021-03-16 12:42:48 +0100

    Add Employee class

---
+++ Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VodakomBlue.Model
+{
+    public class Employee : User
+    {
+        public Address Address { get; set; }
+        public EmployeeType EmployeeType { get; set; } 
+    }
+}
