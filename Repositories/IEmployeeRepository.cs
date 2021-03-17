c0f5859502a1569dcf1b3d469d501596187f09b3
kalacs <divixed12@gmail.com>
2021-03-17 12:39:26 +0100

    Add IEmployeeRepository, EmployeeRepository

---
+++ Repositories/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VodakomBlue.Model;
+
+namespace VodakomBlue.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> GetEmployeeAsync(int employeeId);
+        Task AddEmployeeAsync(Employee employee);
+        void UpdateEmployee(Employee employee);
+        void DeleteEmployee(int employeeId);
+    }
+}
---
+++ Repositories/Implementations/EmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VodakomBlue.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace VodakomBlue.Repositories.Implementations
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public EmployeeRepository(AppDbContext context)
+        {
+            dbContext = context;
+        }
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await dbContext.Employees.AddAsync(employee);
+            await dbContext.SaveChangesAsync();
+        }
+        public void DeleteEmployee(int employeeId)
+        {
+            Employee employeeToDelete = GetEmployeeAsync(employeeId).Result;
+            dbContext.Employees.Remove(employeeToDelete);
+            dbContext.SaveChanges();
+        }
+
+        public async Task<Employee> GetEmployeeAsync(int employeeId)
+        {
+            return await dbContext.Employees.FindAsync(employeeId);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var employeeToUpdate = dbContext.Attach(employee);
+            employeeToUpdate.State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+    }
+}
