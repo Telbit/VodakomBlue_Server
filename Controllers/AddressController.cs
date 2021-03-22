31a9f5cf2629092dc588649e2c7237c05d751eb0
kalacs <divixed12@gmail.com>
2021-03-22 11:59:05 +0100

    Add AddressController API

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VodakomBlue.Model;
+using VodakomBlue.Repositories;
+
+namespace VodakomBlue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+
+        private readonly IAddressRepository addressService;
+
+        public AddressController(IAddressRepository service)
+        {
+            addressService = service;
+        }
+
+        [HttpGet("customerId")]
+        public async Task<IEnumerable<Address>> GetAddresses(int customerId)
+        {
+            return await addressService.GetAddressesAsync(customerId);
+        }
+
+        [HttpPost]
+        public async Task AddAddress(Address newAddress)
+        {
+            await addressService.AddAddressAsync(newAddress);
+        }
+
+        [HttpPut]
+        public void UpdateAddress(Address address)
+        {
+            addressService.UpdateAddress(address);
+        }
+
+        [HttpDelete("addressId")]
+        public void DeleteAddress(int addressId)
+        {
+            addressService.DeleteAddress(addressId);
+        }
+    }
+}
