b6f667b0a59496b82d33962c19666828e7114c18
kalacs <divixed12@gmail.com>
2021-03-22 12:00:08 +0100

    Add EmployeeController API

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VodakomBlue.Repositories;
+using VodakomBlue.Model;
+
+namespace VodakomBlue.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeService;
+
+        public EmployeeController(IEmployeeRepository service)
+        {
+            employeeService = service;
+        }
+
+        [HttpGet("id")]
+        public async Task<Employee> GetEmployee(int employeeId)
+        {
+            return await employeeService.GetEmployeeAsync(employeeId);
+
+        }
+
+        [HttpPost]
+        public async Task AddEmployee(Employee newEmployee)
+        {
+             await employeeService.AddEmployeeAsync(newEmployee);
+        }
+
+        [HttpDelete("id")]
+        public void DeleteEmployee(int employeeId)
+        {
+            employeeService.DeleteEmployee(employeeId);
+        }
+
+        [HttpPut]
+        public void UpdateEmployee(Employee employee)
+        {
+            employeeService.UpdateEmployee(employee);
+        }
+
+    }
+}
