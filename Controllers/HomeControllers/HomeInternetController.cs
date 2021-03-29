ab9a64fe2c37b8eb8cb5ef82797af93e11a53bcf
Telbit <t.t.bende@gmail.com>
2021-03-29 18:27:54 +0200

    Add HomeInternetController

---
+++ Controllers/HomeControllers/HomeInternetController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VodakomBlue.Model.Home;
+using VodakomBlue.Repositories.Implementations;
+
+namespace VodakomBlue.Controllers.HomeControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeInternetController : ControllerBase
+    {
+        private readonly HomeInternetRepository homeInternetRepository;
+
+        public HomeInternetController(HomeInternetRepository homeInternetRepository)
+        {
+            this.homeInternetRepository = homeInternetRepository;
+        }
+
+        [HttpGet("{homeInternetId}")]
+        public async Task<IActionResult> GetHomeInternetPackage(int homeInternetPackageId)
+        {
+            if (homeInternetPackageId <= 0)
+            {
+                return BadRequest("Invalid home internet ID");
+            }
+            return Ok(await homeInternetRepository.GetHomeInternetAsync(homeInternetPackageId));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllHomeInternetPackages()
+        {
+            var homeInternetPackages = await homeInternetRepository.GetAllHomeInternetAsync();
+            if (homeInternetPackages == null)
+            {
+                return NotFound("home internet packages not found");
+            }
+            return Ok(homeInternetPackages);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteHomeInternetPackage(HomeInternet homeInternetPackage)
+        {
+            if (homeInternetPackage == null)
+            {
+                return BadRequest("The provided internet package is null");
+            }
+            homeInternetRepository.DeleteHomeInternet(homeInternetPackage);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddHomeInternetPackage(HomeInternet newHomeInternetPackage)
+        {
+            if (newHomeInternetPackage == null)
+            {
+                return BadRequest("The provided internet package is null");
+            }
+            await homeInternetRepository.AddHomeInternetAsync(newHomeInternetPackage);
+            return Ok();
+        }
+    }
+}
