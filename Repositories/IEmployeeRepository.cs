709afcfa4cbd46e5690b974f322854e9f7530cef
kalacs <divixed12@gmail.com>
2021-04-21 19:41:39 +0200

    Add GetAllEmployees method

--- Repositories/IEmployeeRepository.cs
+++ Repositories/IEmployeeRepository.cs
@@ -9,6 +9,7 @@ namespace VodakomBlue.Repositories
     public interface IEmployeeRepository
     {
         Task<Employee> GetEmployeeAsync(int employeeId);
+        Task<IEnumerable<Employee>> GetEmployeesAsync();
         Task AddEmployeeAsync(Employee employee);
         void UpdateEmployee(Employee employee);
         void DeleteEmployee(Employee employee);
